ace0412579cd8423e93e7186f1eef9a8c19d6a94
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-03-03 16:56:15 +0100

    a lil cleanup + added barebones char n cam

---
+++ Camera/camera_control.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+var inpvec : Vector2 = Vector2(0, 0);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (Settings.debugmode):
+		inpvec = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (inpvec.length() > 0.1):
+			position += inpvec * delta * 33.3;
+			print(position);
